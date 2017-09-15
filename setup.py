8f9f9115a9375a6439e3d3c1d693d1b73e8395a0
Riley L <riley.louvier@homes.com>
2017-09-15 14:49:31 -0400

    Added setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from setuptools import setup
+# SaltPY setup
+setup(
+    name = "SaltPY",
+    version = "0.1",
+    author = "Riley",
+    author_email = "riley@fasterdevops.com",
+    url = "https://github.com/sadminriley/",
+    license = "MIT",
+    install_requires=[
+        "saltpy==0.1"
+    ],
+    dependency_links=[
+        "git+https://github.com/sadminriley/saltpy.git#egg=saltpy-0.1"
+    ]
+)
