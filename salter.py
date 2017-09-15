a4681295092e6b02e193da71d43e4dbac87ef2ef
Riley L <riley.louvier@homes.com>
2017-09-15 13:02:46 -0400

    Added some comments

--- salter.py
+++ salter.py
@@ -3,33 +3,36 @@ import os
 import salt
 import shutil
 from argparse import ArgumentParser
-from subprocess import call
+from subprocess import call as run
+
 __version__ = 'SaltPY 0.1 Alpha'
 __author__ = 'Riley - fasterdevops.github.io'
 
 # Let's set some globals!
-PROVIDER = 'digital_ocean'
+PROVIDER = 'digital_ocean' # Set salt-cloud provider
 class Setup(object):
-
+    ''' Class to setup salt master, minions, and download formulas from Riley's github'''
     def __init__(self):
         self.formula_repo = 'git clone https://github.com/sadminriley/saltstack.git'
         self.get_bootstrap = 'curl -L https://bootstrap.saltstack.com -o install_salt.sh'
         self.install_master = 'sh install_salt.sh -P -M'
         self.install_minion = 'sh install_salt.sh -P'
-        self.config_dir = '/etc/salt'
-        self.formula_dir = '/srv/salt'
+        self.config_dir = '/etc/salt'       # Default Salt configuration directory
+        self.formula_dir = '/srv/salt'      # Default Salt formulas directory
 
     def master_setup(self):
-        subprocess.call(self.get_bootstrap, shell=True)
-        subprocess.call(self.install_master, shell=True)
-        subprocess.call(self.git clone, cwd=self.formula_dir)
+        ''' Using run to execute shell commands'''
+        run(self.get_bootstrap)
+        run(self.install_master)
+        run(self.git clone, cwd=self.formula_dir)
         shutil.move('/srv/salt/saltstack/*', self.formula_dir)
 
     def minion_setup(self):
-        subprocess.call(self.get_bootstrap, shell=True)
-        subprocess.call(self.install_minion)
+        run(self.get_bootstrap, shell=True)
+        run(self.install_minion)
 
 class Cloud(object):
+    '''Class for usage with salt-cloud'''
     def __init__(self):
         self.list_profiles = ['salt-cloud',
                               '--list-profiles',
@@ -41,13 +44,13 @@ class Cloud(object):
                             '--list-images',
                             PROVIDER ]
     def list_pro(self):
-        subprocess.call(self.list_profiles)
+        run(self.list_profiles)
 
     def list_sizes(self):
-        subprocess.call(self.list_sizes)
+        run(self.list_sizes)
 
     def list_images(self):
-        subprocess.call(self.list_images)
+        run(self.list_images)
 
 class Master(object):
 
@@ -61,11 +64,11 @@ class Master(object):
                         command ]
 
     def test_ping(self):
-        subprocess.call(self.ping)
+        run(self.ping)
 
     def run_cmd(self):
         command = raw_input('Enter the command you wish to run-:')
-        subprocess.call(self.cmd_run)
+        run(self.cmd_run)
 
 def main():
     '''
