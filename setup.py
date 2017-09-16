db698477fa60cb87b1477add23d595e2416dbb4f
sadminriley <riley@fasterdevops.com>
2017-09-16 22:47:33 +0000

    Geting CI working

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 from setuptools import setup
-# SaltPY setup
+# SaltPY v0.1 setup
 setup(
     name = "SaltPY",
     version = "0.1",
@@ -8,10 +8,7 @@ setup(
     author_email = "riley@fasterdevops.com",
     url = "https://github.com/sadminriley/",
     license = "MIT",
-    install_requires=[
-        "saltpy==0.1",
-        "paramiko==2.2.1"
-    ],
+    install_requires=['paramiko'],
     dependency_links=[
         "git+https://github.com/sadminriley/saltpy.git#egg=saltpy-0.1"
     ]
